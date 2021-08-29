adf36fb27996c14d19ad975a60f5904f9f00fcac
RagulKanna <ragulragi619@gmail.com>
2021-08-29 15:05:48 +0530

    Added:Two Dimensional array

---
+++ src/com/Assignment/Array.java
@@ -0,0 +1,32 @@
+package com.Assignment;
+
+import java.util.Scanner;
+
+public class Array {
+    public static void main(String[] args) {
+        Scanner ar=new Scanner(System.in);
+        System.out.println("\nEnter the no of rows and columns:");
+        System.out.println("rows:");
+        int m =ar.nextInt();
+        System.out.println("column");
+        int n =ar.nextInt();
+        int[][] arr=new int[m][n];
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                System.out.println("Enter the value of "+i+" row , "+j+" column:");
+                arr[i][j]=ar.nextInt();
+            }
+        }
+        System.out.println("\nThe value in matrix is");
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                System.out.println("\narr["+i+"]["+j+"]: "+arr[i][j]);
+            }
+        }
+
+    }
+}
